3bcc70923cc45693f0fb7270341062ec0b30e22e
J. Simon Richard <jsimonrichard@gmail.com>
2023-07-29 16:20:41 -0400

    Some window rules

--- home-manager/desktop/hyprland/window-rules.nix
+++ home-manager/desktop/hyprland/window-rules.nix
@@ -4,4 +4,9 @@ windowrule = size 660,420, ^(.blueman-manager-wrapped)$
 
 windowrule = float, ^(Spotify)$
 windowrule = float, title:zoom
+
+windowrule = float, title:^(Open Folder)
+windowrule = size 960,775, title:^(Open Folder)
+
+windowrule = float, 1Password
 ''
