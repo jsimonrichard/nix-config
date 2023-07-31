e94815a2f04b653436f7b8cc6783b9f62e749062
J. Simon Richard <jsimonrichard@gmail.com>
2023-07-31 17:43:07 -0400

    wallpaper for second monitor

--- home-manager/desktop/hyprpaper.nix
+++ home-manager/desktop/hyprpaper.nix
@@ -3,5 +3,6 @@
   xdg.configFile."hypr/hyprpaper.conf".text = ''
     preload = /home/jsimonrichard/Wallpapers/milky-way-g0a0547461_1920.jpg
     wallpaper = eDP-1, /home/jsimonrichard/Wallpapers/milky-way-g0a0547461_1920.jpg
+    wallpaper = DP-1, /home/jsimonrichard/Wallpapers/milky-way-g0a0547461_1920.jpg
   '';
 }
