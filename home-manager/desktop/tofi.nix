5c880e0b8860209753e1ea97f4acd434cd610a27
J. Simon Richard <jsimonrichard@gmail.com>
2023-07-31 18:13:24 -0400

    Themed tofi

--- home-manager/desktop/tofi.nix
+++ home-manager/desktop/tofi.nix
@@ -6,21 +6,17 @@
   home.sessionVariables.LAUNCHER = "${pkgs.tofi}/bin/tofi-run";
 
   xdg.configFile."tofi/config".text = ''
-    anchor = top
     width = 100%
-    height = 30
-    horizontal = true
-    font-size = 14
-    prompt-text = " run: "
-    font = monospace
-    outline-width = 0
+    height = 100%
     border-width = 0
-    background-color = #000000
-    min-input-width = 120
-    result-spacing = 15
-    padding-top = 0
-    padding-bottom = 0
-    padding-left = 0
-    padding-right = 0
+    outline-width = 0
+    padding-left = 35%
+    padding-top = 35%
+    result-spacing = 25
+    num-results = 5
+    font = monospace
+    background-color = #1a1b26aa
+    text-color = #c0caf5
+    selection-color = #7aa2f7
   '';
 }
