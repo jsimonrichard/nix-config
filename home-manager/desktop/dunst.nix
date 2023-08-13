33b7e108bc2bfde0fabcb6ce1cc4f08444588d70
J. Simon Richard <jsimonrichard@gmail.com>
2023-08-13 14:54:40 -0400

    Dunst styling

--- home-manager/desktop/dunst.nix
+++ home-manager/desktop/dunst.nix
@@ -1,5 +1,23 @@
 { pkgs, ... }: {
   services.dunst = {
     enable = true;
+    settings = {
+      global = {
+        width = 350;
+        height = 150;
+        origin = "top-right";
+        offset = "5x100";
+        corner_radius = 5;
+        font = "Inter 12";
+        separator_color = "#7aa2f733";
+        frame_color = "#7aa2f799";
+        frame_width = 0;
+        background = "#1a1b2699";
+      };
+    };
   };
+
+  wayland.windowManager.hyprland.settings.layerrule = [
+    "blur, notifications"
+  ];
 }
