046fec3d6d3e86c1c0599b1fc45c0ba2c8504adc
J. Simon Richard <jsimonrichard@gmail.com>
2023-08-17 19:10:40 -0400

    Add monitor config for other port

--- home-manager/desktop/hyprland/monitors.nix
+++ home-manager/desktop/hyprland/monitors.nix
@@ -2,6 +2,7 @@
   wayland.windowManager.hyprland.settings.monitor = [
     "eDP-1,1920x1200,0x1080,1"
     "DP-1,preferred,0x0,auto"
+    "DP-3,preferred,0x0,auto"
     ",preferred,auto,auto"
   ];
 }
