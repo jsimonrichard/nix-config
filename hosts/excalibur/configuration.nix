2816a5198cb3fdd92a56bb0f169636cab78de396
J. Simon Richard <jsimonrichard@gmail.com>
2023-11-19 21:29:07 -0500

    Add sound to excalibur

--- hosts/excalibur/configuration.nix
+++ hosts/excalibur/configuration.nix
@@ -53,4 +53,14 @@
 
   # Bluetooth
   hardware.bluetooth.enable = true;
+
+  # Sound
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
 }
